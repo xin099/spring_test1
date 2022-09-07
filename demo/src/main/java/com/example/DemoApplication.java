904a7fa25ba0995d5130c5d572c27f0b3ccaaacf
13014 <13014@163.com>
2022-09-07 19:33:16 +0800

    postman调试2

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -1,9 +1,9 @@
-package com.example.demo;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
--- demo/src/main/java/com/example/controller/UserController.java
+++ demo/src/main/java/com/example/controller/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 
@@ -23,8 +22,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @Resource
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 
     @RequestMapping("/login")
     @ResponseBody
@@ -35,9 +33,10 @@ public class UserController {
         if (user == null){
             System.out.println("登录失败！");
             return new User();
-        }else {
+        }
+        else {
             if (!bCryptPasswordEncoder.matches(password,user.getPassword())){
-                return new User();
+                return  new User();
             }
             //匹配成功
             request.getSession().setAttribute(Constant.USER_SESSION_KEY,user);
